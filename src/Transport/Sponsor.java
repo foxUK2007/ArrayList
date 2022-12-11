e6dcba581b1f036770a3fbda0872b034dc966452
fox <zem_otsp@mail.ru>
2022-12-11 23:16:28 +0500

    ДЗ по ArrayList

---
+++ src/Transport/Sponsor.java
@@ -0,0 +1,36 @@
+package Transport;
+
+public class Sponsor {
+    private String name;
+    private int supportSum;
+
+    public Sponsor(String name, int supportSum) {
+        this.name = name;
+        this.supportSum = supportSum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSupportSum() {
+        return supportSum;
+    }
+
+    public void setSupportSum(int supportSum) {
+        this.supportSum = supportSum;
+    }
+
+    public void sponsorRace() {
+        System.out.println("Спонсирует заезд");
+    }
+
+    @Override
+    public String toString() {
+        return "Спонсор "
+                + name +
+                " сумма поддержки " + supportSum
+                ;
+    }
+}
+
